7b53eefe1611e77bd07846a0a1bd8eb0ab377e09
sharanya chandu <you@example.com>
2023-06-20 23:30:25 +0530

    auto save

--- server.js
+++ server.js
@@ -128,34 +128,30 @@ app.get('/search/:text', (req, res) => {
     }
 });
 
-if (process.env.MONGO == 'true') {
-// set up Mongo
-function mongoConnect() {
-    return new Promise((resolve, reject) => {
-        var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
-        mongoClient.connect(mongoURL, (error, client) => {
-            if(error) {
-                reject(error);
-            } else {
-                db = client.db('catalogue');
-                collection = db.collection('products');
-                resolve('connected');
-            }
-        });
-    });
-}
-}
+// set up for local Mongo
+// Uncomment it when using Mongo on EC2
+// function mongoConnect() {
+//     return new Promise((resolve, reject) => {
+//         var mongoURL = process.env.MONGO_URL || 'mongodb://mongodb:27017/catalogue';
+//         mongoClient.connect(mongoURL, (error, client) => {
+//             if(error) {
+//                 reject(error);
+//             } else {
+//                 db = client.db('catalogue');
+//                 collection = db.collection('products');
+//                 resolve('connected');
+//             }
+//         });
+//     });
+// }
 
-if (process.env.DOCUMENTDB == 'true') {
+// Code for Document DB, 
 function mongoConnect() {
     return new Promise((resolve, reject) => {
     var mongoURL = process.env.MONGO_URL || 'mongodb://username:password@mongodb:27017/catalogue?tls=true&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false';
     var client = mongoClient.connect(mongoURL,
       {
-        // Mutable & Immutable
-        //tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
-        // Container
-        tlsCAFile: `/home/roboshop/catalogue/rds-combined-ca-bundle.pem` //Specify the DocDB; cert
+        tlsCAFile: `/home/roboshop/rds-combined-ca-bundle.pem` //Specify the DocDB; cert to be used, ensure it has to present on server
     }, (error, client) => {
     if(error) {
         reject(error);
@@ -167,8 +163,6 @@ function mongoConnect() {
 });
 });
 }
-}
-
 
 // mongodb connection retry loop
 function mongoLoop() {
